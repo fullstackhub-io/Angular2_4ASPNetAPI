2044f6ad2f794776fcccc62eb78680eb51febd97
fullstackhub-io <fullstackhub@yahoo.com>
2018-03-22 00:05:49 -0500

    Added Part 1
    Added Part 1

---
+++ Angular2MVC_Angular4_P1/Angular2MVC/app/Shared/global.js
@@ -0,0 +1,9 @@
+"use strict";
+var Global = (function () {
+    function Global() {
+    }
+    return Global;
+}());
+Global.BASE_USER_ENDPOINT = 'api/userapi/';
+exports.Global = Global;
+//# sourceMappingURL=global.js.map
