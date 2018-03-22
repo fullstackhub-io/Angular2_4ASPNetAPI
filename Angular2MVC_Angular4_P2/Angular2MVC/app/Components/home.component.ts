617b2e6cdb8382d4cf4bdf64164e158282481067
fullstackhub-io <fullstackhub@yahoo.com>
2018-03-22 00:08:02 -0500

    Added Part 2
    Added Part 2

---
+++ Angular2MVC_Angular4_P2/Angular2MVC/app/Components/home.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    template: `<img src="../../images/users.png" style="text-align:center"/>
+               <!--<button class="btn btn-primary" (click)="IdontExist()">ErrorButton</button>-->
+               `
+})
+
+export class HomeComponent{
+}
---
+++ Angular2MVC_Angular4_P2/Angular2MVC/app/app.module.ts
@@ -0,0 +1,22 @@
+﻿import { NgModule, ErrorHandler } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+import { UserComponent } from './components/user.component';
+import { HomeComponent } from './components/home.component';
+import { UserService} from './Service/user.service'
+import { UserFilterPipe } from './filter/user.pipe'
+import { SearchComponent } from './Shared/search.component';
+import AppErrorHandler from './Shared/errorhandler';
+@NgModule({
+    imports: [BrowserModule, ReactiveFormsModule, HttpModule, routing, Ng2Bs3ModalModule, FormsModule],
+    declarations: [AppComponent, UserComponent, HomeComponent, UserFilterPipe, SearchComponent],
+    providers: [{ provide: ErrorHandler, useClass: AppErrorHandler }, { provide: APP_BASE_HREF, useValue: '/' }, UserService],
+    bootstrap: [AppComponent]
+
+})
+export class AppModule { }
